a3e8bbdea8f514f46de1e36cf3f845840207b7da
PrzemyslawBorczak1 <przemyslawborczak@gmail.com>
2026-06-05 11:33:41 +0200

    Created src folder

---
+++ src/Mentat/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Mentat;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Mentat/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Mentat.Infrastructure.LLM;
+using Mentat.Infrastructure.LLM.Client;
+
+
+namespace Mentat;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object? sender, EventArgs e)
+	{
+       
+        count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
